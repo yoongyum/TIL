d18762fcf9520fced196d743fab72de71a00a719
yoongyum <imshreg@naver.com>
2022-04-08 17:04:28 +0900

    feat: Create HashMap

---
+++ algorithms/Hash/IHashTable.java
@@ -0,0 +1,14 @@
+package Hash;
+
+public interface IHashTable <K ,V>{
+
+    void put(K key, V value);
+
+    V get(K key);
+
+    boolean delete(K key);
+
+    boolean contains(K key);
+
+    int size();
+}
